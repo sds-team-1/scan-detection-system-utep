3ec745048f84891ad038a33ac9190c1fa253ab70
Erik-Mtz <erikrivera40@gmail.com>
2022-03-08 16:27:55 -0700

    able to start vm

--- Helpers/test_VirtualBoxHelper.py
+++ Helpers/test_VirtualBoxHelper.py
@@ -1,13 +1,9 @@
-from VirtualBoxHelper import VirtualBoxInstance
-
-
-
-def main():
-    vb = VirtualBoxInstance()
-    vb.start()
-
-
-
-
-
-main()
+import virtualbox
+print("TESTING")
+vbox = virtualbox.VirtualBox()
+session = virtualbox.Session()
+print("TESTING")
+machine = vbox.find_machine("ubuntu")
+progress = machine.launch_vm_process(session, "gui", []);
+print("TESTING")
+progress.wait_for_completion()
