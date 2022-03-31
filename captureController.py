614a6ea4688539d24f0857dc2983747be86b37fa
Erik-Mtz <erikrivera40@gmail.com>
2022-03-30 19:24:01 -0600

    update capture controller to be a static classi

--- captureController.py
+++ captureController.py
@@ -5,86 +5,101 @@ vimname = "CoreUbuntu"
 vim_username = "cj"
 vim_password = "1386"
 
-def startScenario():
-    '''
-    Runs the 'run bash ~/core/Files/CoreStart' command on the VM
-    '''
-    os.system(f"VBoxManage guestcontrol run --username \"{vim_username}\" --password \"{vim_password}\" bash ~/core/Files/CoreStart")
+class CaptureController:
 
 
-def startService():
-    '''
-    Runs the 'run bash ~/core/Files/StartServices' command on the VM
-    '''
-    os.system(f"VBoxManage guestcontrol \"{vimname}\" run --username \"{vim_username}\" --password \"{vim_password}\" bash ~/core/Files/StartServices")
+    def startScenario():
+        '''
+        Runs the 'run bash ~/core/Files/CoreStart' command on the VM
+        '''
+        os.system(f"VBoxManage guestcontrol run --username \"{vim_username}\" --password \"{vim_password}\" bash ~/core/Files/CoreStart")
 
 
-def cleanup():
-    '''
-    Runs the 'core-cleanup' command on the VM
-    and the
-    'rm -r ~/core/Files/Captures/*' command on the VM
-    '''
-    os.system(f"VBoxManage guestcontrol \"{vimname}\" run --username \"{vim_username}\" --password \"{vim_password}\" core-cleanup")
-    os.system(f"VBoxManage guestcontrol run --username \"{vim_username}\" --password \"{vim_password}\" rm -r ~/core/Files/Captures/*")
+    def startService():
+        '''
+        Runs the 'run bash ~/core/Files/StartServices' command on the VM
+        '''
+        os.system(f"VBoxManage guestcontrol \"{vimname}\" run --username \"{vim_username}\" --password \"{vim_password}\" bash ~/core/Files/StartServices")
 
 
-def run_command(command, args=""):
-    # VBoxManage guestcontrol CoreUbuntu --username cj --password 1386 run /bin/ls  
-    os.system(f"VBoxManage guestcontrol \"{vimname}\" --username \"{vim_username}\" --password \"{vim_password}\" run \"{command}\" {args}")
+    def cleanup():
+        '''
+        Runs the 'core-cleanup' command on the VM
+        and the
+        'rm -r ~/core/Files/Captures/*' command on the VM
+        '''
+        os.system(f"VBoxManage guestcontrol \"{vimname}\" run --username \"{vim_username}\" --password \"{vim_password}\" core-cleanup")
+        os.system(f"VBoxManage guestcontrol run --username \"{vim_username}\" --password \"{vim_password}\" rm -r ~/core/Files/Captures/*")
 
 
-def restoreScenario():
-    cleanup()
-    startScenario()
-    startService()
+    def run_command(command, args=""):
+        # VBoxManage guestcontrol CoreUbuntu --username cj --password 1386 run /bin/ls  
+        os.system(f"VBoxManage guestcontrol \"{vimname}\" --username \"{vim_username}\" --password \"{vim_password}\" run \"{command}\" {args}")
 
-def add_shared_folder(name, hostpath):
-    os.system(f"VBoxManage sharedfolder add \"{vimname}\" --name {name} --hostpath {hostpath} --automount")
+    def run_scenario(xml_as_string):
+        '''
+        Receives an xml string and runs the commands in the scenario
+        '''
+        pass
 
-def startVM():
-    os.system(f"VBoxManage startvm \"{vimname}\"")
+    def restoreScenario():
+        '''
+        Runs commands that allows virtual machine to transition to ready state to run scenario
+        '''
+        pass
+        # cleanup()
+        # startScenario()
+        # startService()
 
-def startVMHeadless():
-    os.system(f"VBoxManage startvm \"{vimname}\" --type headless")
+    def add_shared_folder(name, hostpath):
+        os.system(f"VBoxManage sharedfolder add \"{vimname}\" --name {name} --hostpath {hostpath} --automount")
 
-def emergency_stop():
-    os.system(f"VBoxManage startvm \"{vimname}\" --type emergencystop")
+    def startVM():
+        '''
+        Starts the VM with the name 'CoreUbuntu'
+        '''
+        os.system(f"VBoxManage startvm \"{vimname}\"")
+
+    def startVMHeadless():
+        os.system(f"VBoxManage startvm \"{vimname}\" --type headless")
+
+    def emergency_stop():
+        os.system(f"VBoxManage startvm \"{vimname}\" --type emergencystop")
 
 
-# add logic in case the file is ran from the command line
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("Please provide a command, use -h for help")
-    elif sys.argv[1] == "start":
-        startVM()
-    elif sys.argv[1] == "start-headless":
-        startVMHeadless()
-    elif sys.argv[1] == "start-scenario":
-        startScenario()
-    elif sys.argv[1] == "start-service":
-        startService()
-    elif sys.argv[1] == "cleanup":
-        cleanup()
-    elif sys.argv[1] == "restore":
-        restoreScenario()
-    elif sys.argv[1] == "emergency-stop":
-        emergency_stop()
-    elif sys.argv[1] == "add-shared-folder":
-        add_shared_folder(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == "run":
-        print("Attempting to run command: " + sys.argv[2])
-        if len(sys.argv) == 3:
-            run_command(sys.argv[2])
+    # add logic in case the file is ran from the command line
+    if __name__ == "__main__":
+        if len(sys.argv) == 1:
+            print("Please provide a command, use -h for help")
+        elif sys.argv[1] == "start":
+            startVM()
+        elif sys.argv[1] == "start-headless":
+            startVMHeadless()
+        elif sys.argv[1] == "start-scenario":
+            startScenario()
+        elif sys.argv[1] == "start-service":
+            startService()
+        elif sys.argv[1] == "cleanup":
+            cleanup()
+        elif sys.argv[1] == "restore":
+            restoreScenario()
+        elif sys.argv[1] == "emergency-stop":
+            emergency_stop()
+        elif sys.argv[1] == "add-shared-folder":
+            add_shared_folder(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "run":
+            print("Attempting to run command: " + sys.argv[2])
+            if len(sys.argv) == 3:
+                run_command(sys.argv[2])
+            else:
+                run_command(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            print("""
+            Usage:
+            start-scenario - starts the scenario
+            start-service - starts the services
+            cleanup - cleans up the scenario
+            restore - restores the scenario
+            """)
         else:
-            run_command(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-        print("""
-        Usage:
-        start-scenario - starts the scenario
-        start-service - starts the services
-        cleanup - cleans up the scenario
-        restore - restores the scenario
-        """)
-    else:
-        print("Command not recognized, please use -h or --help for help")
+            print("Command not recognized, please use -h or --help for help")
