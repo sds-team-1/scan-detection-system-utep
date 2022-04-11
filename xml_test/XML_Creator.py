619c1bbbbe7ebd6fa357b608d0cd7d6f7d4fe9d9
Julio A Hernandez <jahernandez14@miners.utep.edu>
2022-04-10 20:36:59 -0600

    working version without comments

--- xml_test/XML_Creator.py
+++ xml_test/XML_Creator.py
@@ -60,13 +60,22 @@ class XmlHelper:
     networks_str = ""
     devices_str = ""
     links_str = ""
+    routerLinkips= []
 
+    link_dict = {}
+
+    # Constcutor to create helper class object
     def __init__(self, input_dict):
         self.input_dict = input_dict
+        self.format_dict()
+        self.create_xml_string()
+        self.create_xml_file()
 
+    # method used to replace strings it created xml string. This is required due to the library limitation that can only transform dictionary to xml. Since dictionaries dont allow duplicated i must do this to force duplicate keys.
     def replace_key_XML(self, new_str, old_str):
         self.xml_str = self.xml_str.replace(new_str, old_str)
 
+    # callable method used to create a string from using the dictionary provided to the obj
     def create_xml_string(self):
         self.xml_str = xmltodict.unparse(self.formatted_dict, short_empty_elements=True)
 
@@ -85,12 +94,15 @@ class XmlHelper:
 
         self.xml_str = self.xml_str[:len(self.xml_str)-11] + self.config
 
+    # callable method used to create an xml file
     def create_xml_file(self):
         xmlfile = open(f"{self.input_dict['name']}.xml", "w")
         xmlfile.write(self.xml_str)
         xmlfile.close()
 
+    # main method that initiates the whole xml creation process 
     def format_dict(self):
+        self.find_subnets()
         self.formatted_dict = {"scenario": {'@name': os.getcwd() + f'\{self.input_dict["name"]}'}}
 
         networks = self.format_networks()
@@ -103,35 +115,65 @@ class XmlHelper:
         links = self.format_links()
         self.formatted_dict["scenario"]["links"] = links["links"]
 
+    def find_subnets(self):
+        self.link_dict
+        for node in self.input_dict["devices"]+self.input_dict["networks"]:
+            ip_split = node['ip'].split(".")
+            subnet = ip_split[0] + "." + ip_split[1] + "." + ip_split[2]
+            originalIP = subnet + ".1"
+            subnet = subnet.replace(".","")
+            if subnet in self.link_dict:
+                node["link"] = subnet
+                self.link_dict[subnet].append(node["id"])
+            else:
+                node["link"] = subnet
+                self.link_dict[subnet] = [node["id"]]
+                self.routerLinkips.append(originalIP)
+        return self.link_dict
+
     def format_links(self):
         links_dict = {"links": {}}
         linksList = self.input_dict["devices"] + self.input_dict["networks"]
-
+        ethCount = 0
         for i in range(len(linksList)):
             name = ""
             if linksList[i]["type"] == "RJ45":
                 name = "enp0s3"
                 self.links_str = self.links_str + xmltodict.unparse(
-                {'link': {'@node1': "99", '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter),"@name": name},
+                {'link': {'@node1': linksList[i]["link"], '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter+100),"@name": name},
                 "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
                 }, short_empty_elements=True).replace('<?xml version="1.0" encoding="utf-8"?>\n', "")
             
                 links_dict["links"].update(
-                {'link': {'@node1': "99", '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter),"@name": name},
+                {'link': {'@node1': linksList[i]["link"], '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter+100),"@name": name},
                 "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
                 })    
 
-            else:
+            elif linksList[i]["type"] == "PC":
                 name = "eth0"
                 self.links_str = self.links_str + xmltodict.unparse(
-                {'link': {'@node1': "99", '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter),"@name": name, "@mac": linksList[i]["mac"], "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "", "@ip6_mask": "64"},
+                {'link': {'@node1': linksList[i]["link"], '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter+100),"@name": name, "@mac": linksList[i]["mac"], "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "", "@ip6_mask": "64"},
                 "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
                 }, short_empty_elements=True).replace('<?xml version="1.0" encoding="utf-8"?>\n', "")
             
                 links_dict["links"].update(
-                {'link': {'@node1': "99", '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter),"@name": name, "@mac": linksList[i]["mac"], "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "", "@ip6_mask": "64"},
+                {'link': {'@node1': linksList[i]["link"], '@node2': linksList[i]["id"],"iface2":{"@id":str(self.id_counter+100),"@name": name, "@mac": linksList[i]["mac"], "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "", "@ip6_mask": "64"},
                 "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
                 })
+            elif linksList[i]["type"] == "SWITCH":
+                name = "eth" + str(ethCount)
+                self.links_str = self.links_str + xmltodict.unparse(
+                {'link': {'@node1': linksList[i]["id"], '@node2': "999","iface2":{"@id":str(self.id_counter+1000),"@name": name, "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "2001::1", "@ip6_mask": "64"},
+                "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
+                }, short_empty_elements=True).replace('<?xml version="1.0" encoding="utf-8"?>\n', "")
+            
+                links_dict["links"].update(
+                {'link': {'@node1': linksList[i]["id"], '@node2': "999","iface2":{"@id":str(self.id_counter+1000),"@name": name, "@ip4": linksList[i]["ip"], "@ip4_mask": linksList[i]["ip4_mask"], "@ip6": "2001::1", "@ip6_mask": "64"},
+                "options":{"@delay": "0", "@bandwith": "0", "@loss": "0.0", "@dup": "0", "@jitter": "0", "@unidirectional": "0", "@buffer": "0"}}
+                })
+                self.id_counter += 1
+                ethCount += 1
+            else: continue
         return links_dict   
 
     def format_devices(self):
@@ -148,6 +190,16 @@ class XmlHelper:
                 {'device': {'@id': devicesList[i]["id"], '@name': devicesList[i]["name"],
                             '@icon': "", "@canvas": "1", "@type": devicesList[i]["type"],
                             "position": self.format_position()}})
+
+        self.devices_str = self.devices_str + xmltodict.unparse(
+            {'device': {'@id': "999", '@name': "R",
+                            '@icon': "", "@canvas": "1", "@type": "router" , "@class" : "", "@image" : "",
+                            "position": {'@x': "385.54595947265625", '@y': "545.1220703125", '@lat': "47.57421120379505", "@lon": "-122.12712909132811", "@alt": "2.0"}}}, short_empty_elements=True).replace(
+            '<?xml version="1.0" encoding="utf-8"?>\n', "")
+        devices_dict["devices"].update(
+            {'device': {'@id': "999", '@name': "R",
+                            '@icon': "", "@canvas": "1", "@type": "router", "@class" : "", "@image" : "",
+                            "position": {'@x': "385.54595947265625", '@y': "545.1220703125", '@lat': "47.57421120379505", "@lon": "-122.12712909132811", "@alt": "2.0"}}})
 
         return devices_dict
 
@@ -166,20 +218,27 @@ class XmlHelper:
                              '@icon': "", "@canvas": "1", "@type": networksList[i]["type"],
                              "position": self.format_position()}})
 
-        self.networks_str = self.networks_str + xmltodict.unparse(
-            {'network': {'@id': "99", '@name': "Switch",
-                            '@icon': "", "@canvas": "1", "@type": "SWITCH",
-                            "position": {'@x': "45.61198425292969", '@y': "347.94708251953125", '@lat': "47.575", "@lon": "-122.1254116826387", "@alt": "2.0"}}}, short_empty_elements=True).replace(
-            '<?xml version="1.0" encoding="utf-8"?>\n', "")
-        network_dict["networks"].update(
-            {'network': {'@id': "99", '@name': "Switch",
-                            '@icon': "", "@canvas": "1", "@type": "SWITCH",
-                            "position": {'@x': "45.61198425292969", '@y': "347.94708251953125", '@lat': "47.575", "@lon": "-122.1254116826387", "@alt": "2.0"}}})
+        count = 1
+        ip = 0
+        for switch in self.link_dict:
+            self.id_counter += 1
+            self.input_dict["networks"].append({"id":switch,"name": "s"+ str(count), "link": "999", "type" : "SWITCH", "ip": self.routerLinkips[ip], "ip4_mask": "24", "ip6":"", "ip6_mask":"64"})    
+            self.networks_str = self.networks_str + xmltodict.unparse(
+                {'network': {'@id': switch, '@name': "s"+ str(count),
+                                '@icon': "", "@canvas": "1", "@type": "SWITCH",
+                                "position": {'@x': "258.0612487792969", '@y': "388.3501281738281", '@lat': "47.57563632286221", "@lon": "-122." + str(132 - (self.id_counter)), "@alt": "2.0"}}}, short_empty_elements=True).replace(
+                '<?xml version="1.0" encoding="utf-8"?>\n', "")
+            network_dict["networks"].update(
+                {'network': {'@id': switch, '@name': "s"+ str(count),
+                                '@icon': "", "@canvas": "1", "@type": "SWITCH",
+                                "position": {'@x': "258.0612487792969", '@y': "388.3501281738281", '@lat': "47.57563632286221", "@lon": "-122." + str(132 - (self.id_counter)), "@alt": "2.0"}}})
+            count += 1
+            ip += 1
         return network_dict
 
     def format_position(self):
         self.id_counter += 1
-        position = {'@x': "0", '@y': "0", '@lat': "47.576", "@lon": "-122." + str(1330 - (self.id_counter*5)), "@alt": "2.0"}
+        position = {'@x': "0", '@y': "0", '@lat': "47.57792708883367", "@lon": "-122." + str(1330 - (self.id_counter*5)), "@alt": "2.0"}
         return position
 
     def get_xml_str(self):
@@ -187,3 +246,55 @@ class XmlHelper:
 
     def get_networks_str(self):
         return self.networks_str
+
+test_input = {"name": "ScenarioA",
+              "networks":[
+                #   {"id": "12",
+                #    "name": "Docker",
+                #    "type": "RJ45",
+                #    "mac": "aa:aa:aa:aa:aa:aa",
+                #    "ip": "10.10.10.10",
+                #    "ip4_mask": "24"},
+                  ],
+              "devices":[
+                  {"id": "14",
+                   "name": "n1s1",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:ac",
+                   "ip": "10.10.1.2",
+                   "ip4_mask": "24"},
+                   {"id": "15",
+                   "name": "n2s1",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:ae",
+                   "ip": "10.10.1.3",
+                   "ip4_mask": "24"},
+                    {"id": "16",
+                   "name": "n3s1",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:ad",
+                   "ip": "10.10.1.4",
+                   "ip4_mask": "24"},
+                   {"id": "17",
+                   "name": "n4s2",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:af",
+                   "ip": "10.10.2.2",
+                   "ip4_mask": "24"},
+                    {"id": "18",
+                   "name": "n5s2",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:1c",
+                   "ip": "10.10.2.4",
+                   "ip4_mask": "24"},
+                   {"id": "19",
+                   "name": "n6s2",
+                   "type": "PC",
+                   "mac": "aa:aa:aa:aa:aa:1e",
+                   "ip": "10.10.2.3",
+                   "ip4_mask": "24"}
+                  ]
+              }
+
+xml = XmlHelper(test_input)
+print(xml.get_xml_str())
