1e7c1c247d77d2bba8492ca17f267eaef79f59fd
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2022-04-16 02:19:08 -0600

    Created model classes for objects

---
+++ Models/modelClasses.py
@@ -0,0 +1,46 @@
+class Workspace:
+    def __init__(self, name: str = '', projects: list = []):
+        self.name = name
+        self.projects = projects
+
+
+class Project:
+    def __init__(self, name: str, max_units: int, scenarios: list = []):
+        self.name = name
+        self.max_units = max_units
+        self.scenarios = scenarios
+
+
+class Scenario:
+    def __init__(self, id, name, networks: list, devices: list, links: list):
+        self.id = id
+        self.name = name
+        self.networks = networks
+        self.devices = devices
+        self.links = links
+
+
+class Node:
+    def __init__(self, id: int, listening: bool, node_type: str, name: str,
+                 IP: str, port: int, MAC: str, network: int):
+        self.id = id
+        self.listening = listening
+        self.type = node_type
+        self.name = name
+        self.IP = IP
+        self.port = port
+        self.MAC = MAC
+        self.network = network
+
+
+class ScannerNode(Node):
+    def __init__(self, id: int, listening: bool, node_type: str, name: str, IP: str, \
+                 port: int, MAC: str, network: int, us_pas, scanner_binary, arguments, \
+                 iterations, max_parallel_runs, end_condition):
+        super().__init__(id, listening, node_type, name, IP, port, MAC, network)
+        self.us_pas = us_pas
+        self.scanner_binary = scanner_binary
+        self.arguments = arguments
+        self.iterations = iterations
+        self.max_parallel_runs = max_parallel_runs
+        self.end_condition = end_condition
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import uuid
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QTreeWidgetItem, QFileDialog, QAction
 
+from Models.modelClasses import Workspace
 from views.addNodeWindow import Ui_addNode_window
 from views.analysisManagerWindow import Ui_AnalysisManagerWindow
 from views.createWorkspace import Ui_newWorkspace_window
@@ -23,54 +24,6 @@ from Controllers.AnalysisManager import SDSAnalysisManager
 from Controllers.CaptureController import CaptureController
 from Controllers.SDSController import SDSController
 from Database.DatabaseHelper import SDSDatabaseHelper
-
-
-class Workspace:
-    def __init__(self, name: str = '', projects: list = []):
-        self.name = name
-        self.projects = projects
-
-
-class Project:
-    def __init__(self, name: str, max_units: int, scenarios: list = []):
-        self.name = name
-        self.max_units = max_units
-        self.scenarios = scenarios
-
-
-class Scenario:
-    def __init__(self, id, name, networks: list, devices: list, links: list):
-        self.id = id
-        self.name = name
-        self.networks = networks
-        self.devices = devices
-        self.links = links
-
-
-class Node:
-    def __init__(self, id: int, listening: bool, node_type: str, name: str,
-                 IP: str, port: int, MAC: str, network: int):
-        self.id = id
-        self.listening = listening
-        self.type = node_type
-        self.name = name
-        self.IP = IP
-        self.port = port
-        self.MAC = MAC
-        self.network = network
-
-
-class ScannerNode(Node):
-    def __init__(self, id: int, listening: bool, node_type: str, name: str, IP: str, \
-                 port: int, MAC: str, network: int, us_pas, scanner_binary, arguments, \
-                 iterations, max_parallel_runs, end_condition):
-        super().__init__(id, listening, node_type, name, IP, port, MAC, network)
-        self.us_pas = us_pas
-        self.scanner_binary = scanner_binary
-        self.arguments = arguments
-        self.iterations = iterations
-        self.max_parallel_runs = max_parallel_runs
-        self.end_condition = end_condition
 
  
 app = QtWidgets.QApplication(sys.argv)
